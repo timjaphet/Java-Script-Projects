c679b3fae3d84b567bd64ab7fa75917bee1ebea6
Timothy Allison <timothyjosephallison@gmail.com>
2022-08-02 03:48:10 -0600

    BASIC JAVASCRIPT PROJECTS SUBMISSION

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -4,10 +4,13 @@ var A = "Hello Again!";
 	document.write(A);
 ;
 var B = "Please continue to page";
-window.alert(B);
+window.alert(B); /*This is an pop up*/
 
 document.write("Remember to say \"please\" and \"thank you.\""+ "-Tim Allison");
 
 var colour1 = "purple";
 	var colour2 = "pink";
 	document.write('<p>colour1: ' + colour1 + '<br>colour2: ' + colour2 + '</p>');
+;
+
+document.write(4+4) /*This is an expresion*/
