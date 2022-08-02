c337f9effe834a2ad8214f54d0bcea866f6d365f
Timothy Allison <timothyjosephallison@gmail.com>
2022-08-02 02:30:59 -0600

    Initial commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+document.write("Hello  World!");
+document.write;
+var A = "Hello Again!";
+	document.write(A);
+;
+var B = "Please continue to page";
+window.alert(B);
+
+document.write("Remember to say \"please\" and \"thank you.\""+ "-Tim Allison");
+
+var colour1 = "purple";
+	var colour2 = "pink";
+	document.write('<p>colour1: ' + colour1 + '<br>colour2: ' + colour2 + '</p>');
