82e1aa8404065413e82ec109a6cc11037a6b014a
Timothy Allison <timothyjosephallison@gmail.com>
2022-08-02 23:06:21 -0600

    concatenation function

---
+++ Project2_functions/JS/java.js
@@ -0,0 +1,5 @@
+function myFunction()   {
+    var sentence = "I am learning"; /*Here on line 2 & 3 I am using the += operator to concatenate my sentence*/
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate") .innerHTML = sentence; /*I am using the get ElementByld methond*/
+}
