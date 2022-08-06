b49f8727e147358729d3152d7178eaec9e420d2e
Timothy Allison <timothyjosephallison@gmail.com>
2022-08-05 18:58:15 -0600

    The real update

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -20,7 +20,7 @@ function myFunction() {
 }
 function count_Function() {
     document.getElementById("Counting").innerHTML = Count();
-    function Count()
+    function Count() {
         var Starting_point = 9;
         function Plus_one() {Starting_point += 1;}
         Plus_one();
